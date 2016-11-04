74d465f4bbd1bebcd143d26f45104d40cb5329c5
415192022 <415192022@qq.com>
2016-11-04 08:52:09 +0800

    Rx

---
+++ app/src/main/java/com/li/pro/view/fragment/FirstSwipeBackFragment.java
@@ -0,0 +1,28 @@
+package com.li.pro.view.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.li.fragmentutils.base.BaseFragment;
+
+import rxop.li.com.rxoperation.R;
+
+
+public class FirstSwipeBackFragment extends BaseFragment {
+    public static FirstSwipeBackFragment newInstance() {
+        Bundle args = new Bundle();
+        FirstSwipeBackFragment fragment = new FirstSwipeBackFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int ftagmentLayout() {
+        return R.layout.fragment_swipe_back_first;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+}
